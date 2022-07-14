468c67059f2875fe9e2e4d8a3a7e3614fef5f14c
David <david.nguyen.thai.ho@gmail.com>
2022-07-15 09:05:33 +1000

    next_possible_move function working

--- lib/knight.rb
+++ lib/knight.rb
@@ -2,29 +2,26 @@
 
 # Knight Class
 class Knight
+  MOVEMENTS = [
+    [1, 2], [1, -2],
+    [-1, 2], [-1, -2],
+    [2, 1], [2, -1],
+    [-2, 1], [-2, -1]
+  ].freeze
+
   def initialize
     @current_position
-    MOVEMENTS = [
-                  [1,2], [1,-2]
-                  [-1,2], [-1,-2]
-        
-                ]
   end
 
+  # Returns an array of valid next possible positions
   def next_possible_move(game, current_position)
     x = current_position[0]
     y = current_position[1]
 
-    indices = [
-      [x - 1, y + 2], [x - 1, y - 2],
-      [x + 1, y + 2], [x + 1, y - 2],
-      [x - 2, y + 1], [x - 2, y - 1],
-      [x + 2, y + 1], [x - 2, y - 1]
-    ]
+    raw = MOVEMENTS.map do |coordinate|
+      [coordinate[0] + x, coordinate[1] + y]
+    end
 
-    indices.select { |coordinate| coordinate unless game.invalid_index?(coordinate) }
+    raw.select { |coordinate| coordinate unless game.invalid_index?(coordinate) }
   end
-
-
 end
-
